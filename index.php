4c16edfad1196135b41547c2df3491289919744e
Javier Flores <javierfh1404@gmail.com>
2017-03-31 10:46:31 -0600

    header

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenido</title>
+
+	  <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+</head>
+<body>
+	<?php include '/header.php';?>
+	<main>
+		<div>
+			<h1>Bienvenido</h1>
+		</div>
+
+	</main>
+</body>
+</html>
