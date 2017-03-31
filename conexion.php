6048274c8c5d34b465606d9caacdd8784eec91f8
Edgardo RD <edgardorito@gmail.com>
2017-03-31 10:02:18 -0600

    Nuevo

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor="localhost";
-$user="jeje";
-$clave="uno";
-$manejador=@mysql_connect($servidor,$user,$clave);
-$basedeDatos="reingeniera";
-//se establece la conexion
-mysql_select_db($basedeDatos,$manejador);
-?>
