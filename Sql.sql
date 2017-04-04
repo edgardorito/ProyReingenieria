32eca418d2b121868d4ad08aaa6ed3e8fc26487c
Edgardo RD <edgardorito@gmail.com>
2017-04-03 19:45:47 -0500

    Nuevo sql

--- Sql.sql
+++ Sql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `proyreing`.`registroalumno` (
+CREATE TABLE `proyreing`.`usuarios` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(50) NOT NULL,
   `apellido` VARCHAR(50) NOT NULL,
@@ -7,3 +7,14 @@ CREATE TABLE `proyreing`.`registroalumno` (
   `email` VARCHAR(15) NOT NULL,
   `contrasena` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
+
+CREATE TABLE IF NOT EXISTS `proyreing`.`cursos` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `idptc` INT(20) NOT NULL,
+  `cuatrimestre` VARCHAR(45) NOT NULL,
+  `materia` VARCHAR(45) NOT NULL,
+  `horario` VARCHAR(45) NOT NULL,
+  `grupo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
